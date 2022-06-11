0c037a8919884245e893d9f573b0955055a8505d
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-06-11 16:29:10 -0700

    added css and Js rules

---
+++ weather-dashboard.js
@@ -0,0 +1,6 @@
+var searchFormEl = document.querySelector("#search-label");
+var citiesContainer = document.querySelector("#cities-container");
+var buttonEntryEl = document.querySelector("#location");
+var mapContainerEl = document.querySelector("#map-container");
+var mapSearchTerm = document.querySelector("#map-search-term");
+
