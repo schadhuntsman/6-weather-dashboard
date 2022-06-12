98098ed5759859da24114243fe88b8688a44fbc2
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-06-12 07:49:44 -0700

    latest

--- weather-dashboard.js
+++ weather-dashboard.js
@@ -1,6 +1,34 @@
 var searchFormEl = document.querySelector("#search-label");
 var citiesContainer = document.querySelector("#cities-container");
-var buttonEntryEl = document.querySelector("#location");
+var locationEntryEl = document.querySelector("#location");
 var mapContainerEl = document.querySelector("#map-container");
 var mapSearchTerm = document.querySelector("#map-search-term");
 
+var formSubmissionPiece = function(event) {
+//prevent page from refreshing
+event.preventDefault();
+
+//get value from location input
+var locationEntry = locationEntryEl.value.trim();
+
+if (location) {
+    getLocationInfo(location);
+
+    //clear content
+    mapContainerEl.textContent = "";
+    locationEntryEl.value = "";
+} else {
+    alert("Please enter a location");
+}
+
+};
+
+// var submitClickAction = function(event) {
+//     //getting the city attribue from clicked element
+//     var cities = event.target.getAttribute("cities-container")
+// }
+
+var getLocations = function(local) {
+    //format the weather api url
+    var weatherApiUrl = ""
+}
