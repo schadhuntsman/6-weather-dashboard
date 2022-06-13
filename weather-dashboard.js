5d162a7405a08b9eed75b1a435b0e2056c29bca7
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-06-12 19:27:36 -0700

    added logic to js

--- weather-dashboard.js
+++ weather-dashboard.js
@@ -21,7 +21,8 @@ if (location) {
     alert("Please enter a location");
 }
 
-};
+
+}
 
 // var submitClickAction = function(event) {
 //     //getting the city attribue from clicked element
@@ -30,5 +31,48 @@ if (location) {
 
 var getLocations = function(local) {
     //format the weather api url
-    var weatherApiUrl = ""
-}
+    var weatherApiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key" + local;
+
+
+//make a get request to the url
+fetch(getLocations) 
+.then(function(response)  {
+    //request was successful
+    if (response.ok) {
+    console.log(response);
+    response.json().then(function(data) {
+        console.log(data);
+        showLocation(data, user);
+    });
+    } else {
+        alert('Error: location information not found');
+    }
+    })
+    .catch(function(error) {
+        alert('Unable to load site')
+    });
+};
+    var showLocation = function(locals, localSearch) {
+        //check if api returned any locations
+        if (locals.length === 0) {
+            mapContainerEl.textContent = 'No cities found';
+        } return;
+  
+
+    mapSearchTerm.textContent = localSearch;
+
+    //create container
+    var mapEl = document.createElement('div');
+    mapEl.classList = "flex-row justify-space-between align-center list-item";
+
+    //create a span to hold city name
+    var mapTitle = document.createElement('span');
+    mapTitle.textContent = cityName;
+
+    //append to container
+    mapEl.appendChild(mapTitle);
+
+
+};
+
+
